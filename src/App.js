3d64c67bfe772643f773717d010dc9ab5b1403a3
cfinn16 <connorfinnegan16@gmail.com>
2019-02-21 21:15:55 -0500

    redux implemented for initial job fetch

--- src/App.js
+++ src/App.js
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import JobsContainer from './JobsContainer.js'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  state = {
-    jobs: []
-  }
 
   componentDidMount() {
     fetch('http://localhost:3001/api/v1/jobs')
       .then(res => res.json())
-      .then(jobs => {
-        this.setState({jobs})
+      .then(fetchedJobs => {
+        this.props.loadJobs(fetchedJobs)
       })
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
           <h1 style={{textAlign: 'center'}}>
             My Job Board
           </h1>
-          <JobsContainer jobs={this.state.jobs}/>
+          <JobsContainer jobs={this.props.jobs}/>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {jobs: state.jobs}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadJobs: (jobs) => dispatch({ type: 'LOAD_JOBS', jobs: jobs})
+  }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
